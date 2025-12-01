d8c179fcdd571d9e33a57247a7df01fd481ac4e3
dyla20044-gif <dyla20044@gmail.com>
2025-11-30 23:36:42 -0500

    Update server.js

--- ala-cine-backend/server.js
+++ ala-cine-backend/server.js
@@ -45,7 +45,7 @@ const ADMIN_CHAT_ID = parseInt(process.env.ADMIN_CHAT_ID, 10);
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
 let GLOBAL_STREAMING_ACTIVE = true;
-const BUILD_ID_UNDER_REVIEW = 10; 
+const BUILD_ID_UNDER_REVIEW = 11; 
 
 const MONGO_URI = process.env.MONGO_URI;
 const MONGO_DB_NAME = process.env.MONGO_DB_NAME || 'sala_cine';
